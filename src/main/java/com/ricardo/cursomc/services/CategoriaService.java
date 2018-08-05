9453c8fa29afdb3378bc6153b9c993db8e1fa808
Ricardo Batista <=>
2018-08-05 11:52:25 +0100

    Delete Categoria com delete

--- src/main/java/com/ricardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/ricardo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ricardo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ricardo.cursomc.domain.Categoria;
 import com.ricardo.cursomc.repositories.CategoriaRepository;
+import com.ricardo.cursomc.services.exceptions.DataIntegrityException;
 import com.ricardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class CategoriaService {
 		
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		
+		find(id);
+		try {
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
